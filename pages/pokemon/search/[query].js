16ab92f3e8b766339690bb7b7ef28913921ad905
Andrew <ashewx@gmail.com>
2025-08-13 23:46:50 -0700

    Make buildable

---
+++ pages/pokemon/search/[query].js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+import AppLayout from '@components/AppLayout'
+
+export default function SearchCard() {
+  return (
+    <>
+      <Head>
+        <title>SearchCard</title>
+      </Head>
+
+      <AppLayout>
+        <div>
+          SearchCard here
+        </div>
+      </AppLayout>
+    </>
+  )
+}
